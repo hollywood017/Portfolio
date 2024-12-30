650058e101b2b6409ce2e8c1fd8941069d66069d
Nick <nhoatz@gmail.com>
2024-12-30 12:21:27 -0500

    Resolve merge conflicts

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container text-center mt-5">
+            <h1>Welcome to My Portfolio</h1>
+            <p>Explore my projects and learn more about my work.</p>
+        </div>
+    );
+};
+export default Home;
