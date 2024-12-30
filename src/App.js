ffe481d7446440566c26e7708a8782cdefcdfc0a
Nick <nhoatz@gmail.com>
2024-12-30 16:20:40 -0500

    debugged routing issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import Projects from './Projects';
